75201715e4b6e0b77efd144747761d005e39c68a
Peerapong Rithisith <p.rithisith@hotmail.com>
2018-07-10 14:22:28 +0100

    add Hystrix, Hystrix dashboard and commands to currency conversion service

--- currency-conversion-service/src/main/java/com/peterith/currencymicroservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/peterith/currencymicroservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,15 +1,13 @@
 package com.peterith.currencymicroservices.currencyconversionservice;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class CurrencyConversionController {
@@ -17,6 +15,11 @@ public class CurrencyConversionController {
     @Autowired
     private CurrencyExchangeServiceProxy proxy;
 
+    @HystrixCommand(fallbackMethod ="convertCurrentOffline", commandProperties = {
+            @HystrixProperty(name="circuitBreaker.requestVolumeThreshold", value="10"),
+            @HystrixProperty(name="circuitBreaker.sleepWindowInMilliseconds", value="10000"),
+            @HystrixProperty(name="circuitBreaker.errorThresholdPercentage", value="40")
+    })
     @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to,
                                                   @PathVariable BigDecimal quantity) {
@@ -25,5 +28,12 @@ public class CurrencyConversionController {
 
         return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
                 quantity.multiply(response.getConversionMultiple()), response.getPort());
+    }
+
+    @GetMapping("/currency-converter-offline/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversionBean convertCurrentOffline(@PathVariable String from, @PathVariable String to,
+                                                        @PathVariable BigDecimal quantity) {
+        return new CurrencyConversionBean(404L, from, to, new BigDecimal(100), quantity,
+                quantity.multiply(new BigDecimal(100)), 0);
     }
 }
