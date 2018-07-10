9bcda783b1228638620426ed080c154e068879fb
Peerapong Rithisith <p.rithisith@hotmail.com>
2018-07-10 13:16:23 +0100

    fix bugs on Feign and Ribbon

--- currency-conversion-service/src/main/java/com/peterith/currencymicroservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/peterith/currencymicroservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -5,8 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="currency-exchange-services")
-@RibbonClient(name="currency-exchange-services")
+@FeignClient(name="currency-exchange-service")
+@RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
