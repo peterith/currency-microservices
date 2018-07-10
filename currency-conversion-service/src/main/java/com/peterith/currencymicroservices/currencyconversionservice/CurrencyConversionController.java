3f6d259a705de54a9b260a081a0cfabf8ec6f81b
Peerapong Rithisith <p.rithisith@hotmail.com>
2018-07-10 11:19:21 +0100

    add functionality, Feign and Robbin to courrency conversion service

---
+++ currency-conversion-service/src/main/java/com/peterith/currencymicroservices/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,29 @@
+package com.peterith.currencymicroservices.currencyconversionservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class CurrencyConversionController {
+
+    @Autowired
+    private CurrencyExchangeServiceProxy proxy;
+
+    @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to,
+                                                  @PathVariable BigDecimal quantity) {
+
+        CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
+
+        return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
+                quantity.multiply(response.getConversionMultiple()), response.getPort());
+    }
+}
---
+++ currency-conversion-service/src/main/java/com/peterith/currencymicroservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -0,0 +1,15 @@
+package com.peterith.currencymicroservices.currencyconversionservice;
+
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name="currency-exchange-services")
+@RibbonClient(name="currency-exchange-services")
+public interface CurrencyExchangeServiceProxy {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from,
+                                                        @PathVariable("to") String to);
+}
