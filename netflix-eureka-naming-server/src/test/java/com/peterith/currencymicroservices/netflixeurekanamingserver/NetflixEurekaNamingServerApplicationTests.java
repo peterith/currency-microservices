91fd3b860af7a4b4a2653cf54c954b918519bdf1
Peerapong Rithisith <p.rithisith@hotmail.com>
2018-07-10 11:37:21 +0100

    rename test packages

--- netflix-eureka-naming-server/src/test/java/com/peterith/currencymicroservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
+++ netflix-eureka-naming-server/src/test/java/com/peterith/currencymicroservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.peterith.microservicesdemo.netflixeurekanamingserver;
+package com.peterith.currencymicroservices.netflixeurekanamingserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
--- spring-cloud-config-server/src/test/java/com/peterith/currencymicroservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
+++ spring-cloud-config-server/src/test/java/com/peterith/currencymicroservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.peterith.microservicesdemo.springcloudconfigserver;
+package com.peterith.currencymicroservices.springcloudconfigserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
