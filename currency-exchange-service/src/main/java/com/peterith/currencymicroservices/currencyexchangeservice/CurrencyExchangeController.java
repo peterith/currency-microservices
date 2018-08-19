34af1e848ee61077e9eac3875728c9c7caab948c
Peerapong Rithisith <p.rithisith@hotmail.com>
2018-08-19 22:07:09 +0100

    fix bug on returning port numer

--- currency-exchange-service/src/main/java/com/peterith/currencymicroservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/peterith/currencymicroservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,6 +1,7 @@
 package com.peterith.currencymicroservices.currencyexchangeservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,17 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class CurrencyExchangeController {
 
     private final CurrencyExchangeService currencyExchangeService;
-
+    private final Environment environment;
+    
     @Autowired
-    public CurrencyExchangeController(CurrencyExchangeService currencyExchangeService) {
+    public CurrencyExchangeController(CurrencyExchangeService currencyExchangeService, Environment environment) {
         this.currencyExchangeService = currencyExchangeService;
+        this.environment = environment;
     }
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public ResponseEntity getExchangeValue(@PathVariable String from, @PathVariable String to) {
 
         return currencyExchangeService.findByFromAndTo(from, to)
-                .map(exchangeValue -> ResponseEntity.ok(exchangeValue))
+                .map(exchangeValue -> {
+                    exchangeValue.setPort(Integer.parseInt(environment.getProperty("server.port")));
+                    return ResponseEntity.ok(exchangeValue);
+                })
                 .orElse(ResponseEntity.notFound().build());
     }
 }
